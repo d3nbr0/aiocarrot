bc49cd8f73326882bcafe7a65e95d31640a01415
d3nbr0 <denchik.ktm@gmail.com>
2024-12-13 15:15:28 +0300

    (feat) shutdown mechanism
    (feat) shutdown mechanism

--- aiocarrot/carrot.py
+++ aiocarrot/carrot.py
@@ -4,7 +4,7 @@ from loguru import logger
 
 from typing import Optional, TYPE_CHECKING
 
-import asyncio, aio_pika, ujson, uuid
+import asyncio, aio_pika, ujson, uuid, copy, signal
 
 if TYPE_CHECKING:
     from aiormq.abc import ConfirmationFrameType
@@ -17,6 +17,7 @@ class Carrot:
 
     _url: str
     _queue_name: str
+    _tasks: list[asyncio.Task]
     _is_consumer_alive: bool = False
     _consumer: Optional['Consumer'] = None
     _connection: Optional['aio_pika.abc.AbstractConnection'] = None
@@ -32,6 +33,7 @@ class Carrot:
         """
 
         self._url = url
+        self._tasks = []
         self._queue_name = queue_name
 
     async def send(self, _cnm: str, **kwargs) -> 'ConfirmationFrameType':
@@ -85,6 +87,9 @@ class Carrot:
             raise RuntimeError('Consumer is not registered. Please, specify using following method: '
                                '.setup_consumer(consumer)')
 
+        if self._is_consumer_alive:
+            raise RuntimeError('Consumer loop is already running')
+
         logger.info('Starting aiocarrot with following configuration:')
         logger.info('')
         logger.info(f'> Queue: {self._queue_name}')
@@ -96,26 +101,28 @@ class Carrot:
         logger.info('')
         logger.info('Starting listener loop...')
 
-        raise_loop_error = None
+        signal.signal(signal.SIGINT, self._exit_signal_handler)
+
+        self._is_consumer_alive = True
+        consumer_task = asyncio.create_task(self._consumer_loop())
 
         try:
-            await self._consumer_loop()
-        except KeyboardInterrupt:
-            pass
-        except asyncio.CancelledError as e:
-            raise_loop_error = e
-        except BaseException:
-            logger.trace('An unhandled error occurred while the consumer was working')
-        finally:
-            logger.info('Shutting down...')
+            while self._is_consumer_alive:
+                if consumer_task.done():
+                    logger.critical(
+                        f'An unhandled error occurred while the consumer was working: '
+                        f'{str(consumer_task.exception())}'
+                    )
+                    break
 
-            await self._channel.close()
-            await self._connection.close()
+                await asyncio.sleep(1)
+        except asyncio.CancelledError:
+            logger.info('Shutdown signal received')
 
-            logger.info('Good bye!')
+        if not consumer_task.done():
+            consumer_task.cancel()
 
-        if raise_loop_error is not None:
-            raise raise_loop_error
+        await self._shutdown()
 
     async def _consumer_loop(self) -> None:
         """
@@ -124,21 +131,16 @@ class Carrot:
         :return:
         """
 
-        if self._is_consumer_alive:
-            raise RuntimeError('Consumer loop is already running')
-
-        if not self._consumer:
-            raise RuntimeError('Consumer is not registered. Please, specify using following method: '
-                               '.setup_consumer(consumer)')
-
         queue = await self._get_queue()
 
         logger.info('Consumer is successfully connected to queue')
 
         async with queue.iterator() as queue_iterator:
-            self._is_consumer_alive = True
-
             async for message in queue_iterator:
+                for task in copy.copy(self._tasks):
+                    if task.done():
+                        self._tasks.remove(task)
+
                 async with message.process():
                     decoded_message: str = message.body.decode()
 
@@ -147,7 +149,7 @@ class Carrot:
 
                         assert isinstance(message_payload, dict)
                     except ujson.JSONDecodeError:
-                        logger.trace(f'Error receiving the message (failed to receive JSON): {decoded_message}')
+                        logger.error(f'Error receiving the message (failed to receive JSON): {decoded_message}')
                         continue
 
                     message_id = message_payload.get('_cid')
@@ -172,11 +174,13 @@ class Carrot:
                     del message_payload['_cid']
                     del message_payload['_cnm']
 
-                    asyncio.create_task(self._consumer.on_message(
+                    task = asyncio.create_task(self._consumer.on_message(
                         message_id,
                         message_name,
                         **message_payload,
                     ))
+
+                    self._tasks.append(task)
 
     async def _get_queue(self) -> 'aio_pika.abc.AbstractQueue':
         """
@@ -215,6 +219,38 @@ class Carrot:
             self._connection = await aio_pika.connect_robust(url=self._url)
 
         return self._connection
+
+    async def _shutdown(self) -> None:
+        """
+        Shutdown carrot application
+
+        :return:
+        """
+
+        pending_tasks = [x for x in self._tasks if not x.done()]
+
+        if len(pending_tasks) > 0:
+            logger.info(f'Waiting for {len(pending_tasks)} tasks...')
+            await asyncio.gather(*pending_tasks)
+
+        try:
+            await self._channel.close()
+        except:
+            pass
+
+        try:
+            await self._connection.close()
+        except:
+            pass
+
+        logger.info('Good bye!')
+
+    def _exit_signal_handler(self, sig, frame) -> int:
+        logger.info('Shutdown signal received')
+
+        self._is_consumer_alive = False
+
+        return 0
 
 
 __all__ = (
