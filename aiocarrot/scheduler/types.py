7c6bb29e083b26020ffab34d01983cb001c7d45e
d3nbr0 <main@denbro.ru>
2025-02-05 02:26:06 +0300

    misc: change start time for cron datetime retriever

--- aiocarrot/scheduler/types.py
+++ aiocarrot/scheduler/types.py
@@ -22,8 +22,11 @@ class Task:
     def __init__(self, carrot: 'Carrot', message: 'Message') -> None:
         self._carrot = carrot
         self._message = message
-        self._cron = croniter(message.schedule, datetime.now(timezone.utc))
-        self._sync()
+
+        now = datetime.now(timezone.utc)
+
+        self._cron = croniter(message.schedule, now)
+        self._sync(now)
 
     async def run(self):
         logger.info(f'Scheduled task <{self._message.name}> has been queued')
@@ -32,7 +35,7 @@ class Task:
 
     def is_due(self, now: datetime) -> bool:
         if self._resync_at_next_tick:
-            self._sync()
+            self._sync(now)
 
         return now >= self._next_run
 
@@ -40,8 +43,8 @@ class Task:
     def next_run(self) -> datetime:
         return self._next_run
 
-    def _sync(self) -> None:
-        self._next_run = self._cron.get_next(datetime)
+    def _sync(self, start_datetime: datetime) -> None:
+        self._next_run = self._cron.get_next(datetime, start_time=start_datetime)
         self._resync_at_next_tick = False
 
 
